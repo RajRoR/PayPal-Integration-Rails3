041c69270c556f62e4c2a00427bccc3ec5f63319
Raj <raj_abc38@yahoo.com>
2012-09-20 02:37:09 +0530

    First Commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,96 @@
+class ProductsController < ApplicationController
+  # GET /products
+  # GET /products.json
+  def index
+    @products = Product.all
+    flash[:notice] = "Your Transaction is #{params[:st]} for Amount of $#{params[:amt]}. Thank You for shopping." if params[:st]
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
+  end
+
+  # GET /products/1
+  # GET /products/1.json
+  def show
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @product }
+    end
+  end
+
+  # GET /products/new
+  # GET /products/new.json
+  def new
+    @product = Product.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @product }
+    end
+  end
+
+  # GET /products/1/edit
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  # POST /products
+  # POST /products.json
+  def create
+    @product = Product.new(params[:product])
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /products/1
+  # PUT /products/1.json
+  def update
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :ok }
+    end
+  end
+
+  def notification
+    #handle for old deals.
+    if params[:item_number1] && !params[:item_number1].empty?
+      #paypal sends an IPN even when the transaction is voided.
+      if params[:payment_status] != 'Voided'
+        @product = Product.find(params[:item_number1].to_i) rescue nil
+        @product.orders.build(:quantity => 1, :amount => params[:mc_gross_1], :status => params[:payment_status]).save unless @product.nil?
+      end
+    end
+    render :nothing => true
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalDemo::Application.config.secret_token = 'a067dd904a9f526352b0243011d0dadf55ceb7d274d4a59649b6e61c29e71359c180d17bc4f4ca79ec7fbd743cdbc64c8338ed052d74d130dc2efd09fc0efc69'
